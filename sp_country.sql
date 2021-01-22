70f8997846a6d8ef20e395f56e0681d7b212e7c6
LironBayda <34834883+LironBayda@users.noreply.github.com>
2021-01-22 21:09:45 +0200

    Update sp_country.sql

--- sp_country.sql
+++ sp_country.sql
@@ -35,14 +35,46 @@ $$ LANGUAGE plpgsql;
 
 
 
-CREATE OR REPLACE FUNCTION remove_country(country_id int)
+CREATE OR REPLACE FUNCTION remove_country(remove_id int)
 returns void
- AS
+ as
     $$
-    BEGIN
-        delete from countries where id=country_id;
+    declare 
+             id_of_flight_with_aircompany_that_has_remove_id  int;
+            
+           id_of_aircompany_with_remove_id int;
+    
+            id_of_flight_with_remove_id int;
+    
+    begin
+	    
+	    select id  into id_of_flight_with_aircompany_that_has_remove_id from  flight  where airline_company_id in 
+                   (select id from airline_company 
+                        where country_id=remove_id );
+         
+          select id  into id_of_aircompany_with_remove_id
+         from airline_company 
+                     where id =remove_id;
+                     
+           select id  into id_of_flight_with_remove_id 
+           from flight 
+            where origin_country_id =remove_id or destination_country_id =remove_id;
+	    
+	    delete from  tickets  where flight_id in 
+         (id_of_flight_with_aircompany_that_has_remove_id) or 
+         flight_id in (id_of_flight_with_remove_id);
+
+	    delete from  flight  where airline_company_id  in
+         (id_of_Aircompany_with_remove_id) or 
+          origin_country_id =remove_id or 
+         destination_country_id =remove_id;
+        
+       delete  from airline_company where country_id =remove_id;
+       delete from countries where id=remove_id;
+
     END;
 $$ LANGUAGE plpgsql;
+
 
 
 CREATE OR REPLACE FUNCTION update_country(name text, country_id int)
