063e8b0011af969e066bd4268b4ddda4c3f49fa9
LironBayda <34834883+LironBayda@users.noreply.github.com>
2021-01-25 23:59:06 +0200

    Update sp_country.sql

--- sp_country.sql
+++ sp_country.sql
@@ -1,17 +1,16 @@
-CREATE OR REPLACE FUNCTION get_country(country_id int )
-returns TABLE(id_num int,country_name text)
+CREATE OR REPLACE FUNCTION get_country(country_id bigint )
+returns TABLE(id bigint,country_name text)
  AS
     $$
     BEGIN
         RETURN QUERY
-        select * from countries where id = country_id ;
+        select * from countries c where c.id = country_id ;
        END;
 $$ LANGUAGE plpgsql;
 
 
-
 CREATE OR REPLACE FUNCTION get_countries( )
-returns TABLE(id_num int,country_name text)
+returns TABLE(id bigint,country_name text)
  AS
     $$
     BEGIN
@@ -19,7 +18,6 @@ returns TABLE(id_num int,country_name text)
          select * from countries;
     END;
 $$ LANGUAGE plpgsql;
-
 
 
 CREATE OR REPLACE FUNCTION add_country(name text )
@@ -32,44 +30,43 @@ returns void
 $$ LANGUAGE plpgsql;
 
 
-
-
-
-CREATE OR REPLACE FUNCTION remove_country(remove_id int)
+CREATE OR REPLACE FUNCTION remove_country(remove_id bigint)
 returns void
  as
     $$
     declare 
-             id_of_flight_with_aircompany_that_has_remove_id  int;
+             id_of_flight_with_aircompany_that_has_remove_id  bigint;
             
-           id_of_aircompany_with_remove_id int;
+           id_of_aircompany_with_remove_id bigint;
     
-            id_of_flight_with_remove_id int;
+            id_of_flight_with_remove_id bigint;
     
     begin
 	    
-	    select id  into id_of_flight_with_aircompany_that_has_remove_id from  flight  where airline_company_id in 
-                   (select id from airline_company 
-                        where country_id=remove_id );
+	    
          
           select id  into id_of_aircompany_with_remove_id
          from airline_companies 
                      where country_id =remove_id;
+                    
+       select id  into id_of_flight_with_aircompany_that_has_remove_id from  flight  
+       where airline_company_id in 
+                  (id_of_aircompany_with_remove_id);
                      
            select id  into id_of_flight_with_remove_id 
-           from flight 
+           from flights 
             where origin_country_id =remove_id or destination_country_id =remove_id;
 	    
 	    delete from  tickets  where flight_id in 
          (id_of_flight_with_aircompany_that_has_remove_id) or 
          flight_id in (id_of_flight_with_remove_id);
 
-	    delete from  flight  where airline_company_id  in
+	    delete from  flights  where airline_company_id  in
          (id_of_Aircompany_with_remove_id) or 
           origin_country_id =remove_id or 
          destination_country_id =remove_id;
         
-       delete  from airline_company where country_id =remove_id;
+       delete  from airline_companies where country_id =remove_id;
        delete from countries where id=remove_id;
 
     END;
@@ -77,7 +74,7 @@ $$ LANGUAGE plpgsql;
 
 
 
-CREATE OR REPLACE FUNCTION update_country(name text, country_id int)
+CREATE OR REPLACE FUNCTION update_country(country_id bigint,name text )
 returns void
  AS
     $$
@@ -85,6 +82,3 @@ returns void
         update countries set country_name=name where id=country_id;
     END;
 $$ LANGUAGE plpgsql;
-
-
-
