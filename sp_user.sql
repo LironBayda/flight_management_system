bf516b2b7e695db639021f8fec74757f91969fcb
LironBayda <34834883+LironBayda@users.noreply.github.com>
2021-01-26 00:00:57 +0200

    Update sp_user.sql

--- sp_user.sql
+++ sp_user.sql
@@ -1,16 +1,17 @@
 
-CREATE OR REPLACE FUNCTION get_user(user_id int )
-returns TABLE(id_num int,username text,password text, email text, user_role int)
+CREATE OR REPLACE FUNCTION get_user(user_id bigint )
+returns TABLE(id bigint,username text,password text, email text, user_role int)
  AS
     $$
     BEGIN
         RETURN QUERY
-        select * from users  where id = user_id ;
+        select * from users u  where u.id = user_id ;
        END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE OR REPLACE FUNCTION get_users( )
-returns TABLE(id_num int,username text,password text, email text, user_role int)
+returns TABLE(id bigint,username text,password text, email text, user_role int)
  AS
     $$
     BEGIN
@@ -19,7 +20,6 @@ returns TABLE(id_num int,username text,password text, email text, user_role int)
     END;
 $$ LANGUAGE plpgsql;
 
-drop function add_user(_username text,_user_password text, _email text, _user_role int);
 CREATE OR REPLACE FUNCTION add_user(_username text,_user_password text, _email text, _user_role int)
 returns void
  AS
@@ -31,15 +31,19 @@ returns void
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION remove_user(remove_id int)
+select  * from add_user ( '_username1' ,'_password1' , '_email1' ,3); 
+
+select  * from users ; 
+
+CREATE OR REPLACE FUNCTION remove_user(remove_id bigint)
 returns void
  as
     $$
     declare 
 
-           id_of_aircompany_with_remove_id int;
-           id_of_customer_with_remove_id int ;         
-             id_of_flight_with_aircompany_that_has_remove_id  int;            
+           id_of_aircompany_with_remove_id bigint;
+           id_of_customer_with_remove_id bigint ;         
+             id_of_flight_with_aircompany_that_has_remove_id  bigint;            
     
     
     begin
@@ -52,7 +56,7 @@ returns void
          from customers where user_id =remove_id;
                     
          select id  into id_of_flight_with_aircompany_that_has_remove_id
-         from flight where airline_company_id in (id_of_aircompany_with_remove_id);
+         from flights where airline_company_id in (id_of_aircompany_with_remove_id);
                     
                      
 	    
@@ -60,11 +64,11 @@ returns void
          (id_of_flight_with_aircompany_that_has_remove_id) or 
          customer_id in (id_of_customer_with_remove_id);
 
-	    delete from  flight  where airline_company_id  in
+	    delete from  flights  where airline_company_id  in
          (id_of_aircompany_with_remove_id);
       
         
-       delete  from airline_company where user_id =remove_id;
+       delete  from airline_companies where user_id =remove_id;
        delete  from customers where user_id =remove_id;
        delete  from administrators where user_id =remove_id;
       delete from  users where id=remove_id;
@@ -73,13 +77,14 @@ returns void
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION update_user(_id_num int,_username text,_user_password text, _email text, _user_role int)
+CREATE OR REPLACE FUNCTION update_user(_user_id bigint,_username text,_user_password text, _email text, _user_role int)
 returns void
  AS
    $$
     BEGIN
         update users 
-       set id=_id_num ,username=_username ,user_password=_user_password, email=_email , user_role=_user_role
-      where id=_id_num;
+       set username=_username ,user_password=_user_password, email=_email , user_role=_user_role
+      where id=_user_id;
     END;
 $$ LANGUAGE plpgsql;
+
