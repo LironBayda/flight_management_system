077d76192d370a0481f8eb9c4133a246d3a87e03
LironBayda <34834883+LironBayda@users.noreply.github.com>
2021-01-27 22:20:29 +0200

    Update sp_tickt.sql

--- sp_tickt.sql
+++ sp_tickt.sql
@@ -1,3 +1,6 @@
+
+drop  FUNCTION get_ticket
+
 CREATE OR REPLACE FUNCTION get_ticket(ticket_id bigint )
 returns TABLE(id bigint,flight_id bigint,customer_id bigint)
  AS
@@ -8,6 +11,7 @@ returns TABLE(id bigint,flight_id bigint,customer_id bigint)
        END;
 $$ LANGUAGE plpgsql;
 
+select  * from get_ticket(6); 
 
 
 drop function get_tickets( );
@@ -21,26 +25,48 @@ returns TABLE(id bigint,flight_id bigint,customer_id bigint)
     END;
 $$ LANGUAGE plpgsql;
 
+select  * from get_tickets(); 
 
 drop function add_ticket(flight_id bigint,customer_id bigint);
 CREATE OR REPLACE FUNCTION add_ticket(_flight_id bigint,_customer_id bigint)
-returns void
+returns bool
  AS
     $$
-    BEGIN
-        insert into tickets (flight_id ,customer_id )
-       values (_flight_id ,_customer_id );
+    declare 
+    _remaining_tickets int ;
+    begin
+	    
+	    select  remaining_tickets into _remaining_tickets from  flights f where f.id =_flight_id;
+	   
+	   
+	    if _remaining_tickets>0 then 
+              insert into tickets (flight_id ,customer_id )
+               values (_flight_id ,_customer_id );
+      
+               update flights set remaining_tickets =remaining_tickets -1 where id=_flight_id;
+              return true;
+       end if;
+       
+       return false;
     END;
 $$ LANGUAGE plpgsql;
 
+
+select  * from add_ticket (6,2) ; 
+
+select  * from tickets ; 
+
+select  * from flights f2 ; 
 
 
 CREATE OR REPLACE FUNCTION remove_tickt(remove_id int)
 returns void
  as
-    $$    
+    $$   
     begin
-	    
+	 
+           update flights set remaining_tickets =remaining_tickets +1
+           where id=(select flight_id from tickets t where t.id=remove_id );
 	  
          delete from tickets where id =remove_id;     
 
@@ -48,18 +74,45 @@ returns void
     END;
 $$ LANGUAGE plpgsql;
 
+select  * from remove_tickt(2); 
 
+select  * from tickets c ; 
+
+delete from countries where id=9;
 
 CREATE OR REPLACE FUNCTION update_ticket(_id bigint,_flight_id bigint,_customer_id bigint)
 returns void
- AS
+AS
    $$
-    BEGIN
+   declare 
+
+   current_flight_id int;
+
+    begin
+
+	    
+	    -- if we change flight id we need to change the remaining tickets accordenaly--
+	    select flight_id into current_flight_id from tickets t where t.id=_id;
+	    
+	    if _flight_id <> current_flight_id then 
+	  
+	    update flights set remaining_tickets =remaining_tickets +1
+        where id=current_flight_id;
+       
+        update flights set remaining_tickets =remaining_tickets -1
+        where id=_flight_id;
+       
+       end if;
+	    
+	    
         update tickets 
        set flight_id=_flight_id ,customer_id=_customer_id
       where id=_id;
+     
     END;
 $$ LANGUAGE plpgsql;
 
 
+select  * from update_ticket (6,6,1); 
 
+select  * from flights c ;
