673895347a3a0ec7e34ea3e382dca268c6809826
LironBayda <34834883+LironBayda@users.noreply.github.com>
2021-01-28 23:31:59 +0200

    Update sp_airline_company.sql

--- sp_airline_company.sql
+++ sp_airline_company.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION get_airline_company_by_Id(airline_company_id bigint)
+CREATE OR REPLACE FUNCTION get_airline_company_by_id(airline_company_id bigint)
 returns TABLE(id bigint,airline_company_name text,country_id int, user_id bigint)
  AS
     $$ 
